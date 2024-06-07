7200b7cf8b40a69da4520fec1799225d49e5e6ca
arshivam <shivam.armarkar007@gmail.com>
2024-06-07 08:46:46 +0530

    enhanced contact component ui

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,10 @@ function Contact() {
   return (
     <section className=' bg-blue-50'>
         <div className=" min-h-[calc(100vh-70px)] flex ">
-          <div className="video">
+          <div className="relative video">
+            <p className='absolute bottom-0 text-white font-sans italic -rotate-12 m-8'>"Seeking a skilled craftsman to transform your stunning UI designs
+          into a seamlessly functioning reality? Let's bring your vision to life
+          with precision and creativity!"</p>
           <video width={481} autoPlay muted loop={true} >
           <source src="/skillingup.mp4" type="video/mp4" />
           </video>
@@ -12,7 +15,7 @@ function Contact() {
           <div className="contact-me flex flex-col gap-6 justify-center items-center px-24">
             <h2 className='font-bold font-mono text-4xl'>Connect with me</h2>
             <a href="mailto:shivam.armarkar007@gmail.com">
-        <button className="bg-orange-600 hover:bg-orange-400 px-10 py-2 text-white border border-t-0 border-r-4 border-b-4 border-l-0 border-black" type="button">Mail to shivam.armarkar007@gmail.com</button>
+        <button className="bg-black px-10 py-2 text-white rounded-full font-bold" type="button">Mail to shivam.armarkar007@gmail.com</button>
         </a>
         <p>By providing your email, you agree to receive occasional emails from me. These emails may include important updates about my latest projects, insightful blog posts, and relevant replies.</p>
         <p>Your privacy is important to me, and I assure you that I will not share your email address with third parties without your explicit consent.</p>
